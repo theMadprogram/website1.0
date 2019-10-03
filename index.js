8432e2062345e89102bd3cadfb4e66d2d3da06fd
theMadprogram <36056741+theMadprogram@users.noreply.github.com>
2019-10-03 14:25:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function getCredentials(getUsername, getPassword) {
+    var username = document.getElementById(getUsername).value;
+    //alert("Value inside txt box: " + str);
+    var password = document.getElementById(getPassword).value;
+}
+
+document.write(getCredentials().password);
+var json = { "combo": [{}] }
+
+
